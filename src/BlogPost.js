50a81ebe7f2f7a66467879f4e42ebae3dc834169
calamont <cal_lamont@hotmail.com>
2020-12-20 09:46:07 +0000

    removed logging

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -2,12 +2,7 @@ import React from 'react';
 
 const BlogPost = ({ blogText }) => {
   const renderMath = () => {
-    console.log("Not in windowwwww")
-    console.log("MathJax" in window)
-    console.log(Object.getOwnPropertyNames(window).sort())
-    console.log(window)
     if ("MathJax" in window) {
-      console.log("finding mathhhhs")
       window.MathJax.Hub.Queue([
         "Typeset",
         window.MathJax.Hub,
