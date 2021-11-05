e71e4e34c7bab5d0e5097ef7c648224e98af6d28
Callum Lamont <cal_lamont@Callums-MacBook-Pro.local>
2021-11-05 07:00:08 +0000

    updated about page text

--- src/main/About.js
+++ src/main/About.js
@@ -5,14 +5,11 @@ const About = () => {
 
   return (
     <div className="blog-text">
-      {/* <h1>Abo</h1> */}
       <p>
         <br />
         Well heck...<br /><br />
 
         Took you long enough to find this site and say hi!<br /><br />
-
-        No, no, come in. I'm still in the process of getting the place all set up so forgive me if there ain't much to do just yet.<br /><br />
 
         Over on the sidebar is my collection of random side projects I've been toying around with. Also got there a few bits of writings that I scribbled down when feeling serious. Click around and see if anything takes your fancy. More detail is given in the README docs for each section.<br /><br />
 
