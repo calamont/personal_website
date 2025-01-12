60b356b133f50746e972c97eec6c5db34c426707
Callum Lamont <callum.lamont@complyadvantage.com>
2025-01-12 13:08:48 +1100

    fix: added open graph properties to meta tags

--- src/main/BlogPost.js
+++ src/main/BlogPost.js
@@ -10,14 +10,18 @@ const BlogPost = ({ blogDetails }) => {
     .then((text) => setBlogText(text))
     .catch((err) => console.log('ERROR', err));
 
+  const domain = 'callumlamont.com';
+  const previewImgUrl = `https://${domain}/blog/images/preview/${blogDetails.slug}.png`;
+  const pageUrl = `https://${domain}/blog/${blogDetails.slug}`;
+
   return (
     <div>
-      <meta name="title" content={blogDetails.title} />
-      <meta name="og:title" content={blogDetails.title} />
-      <meta name="description" content={blogDetails.description} />
-      <meta name="og:description" content={blogDetails.description} />
-      <meta name="keywords" content={blogDetails.keywords} />
-      <meta name="og:keywords" content={blogDetails.keywords} />
+      <meta name="title" property="og:title" content={blogDetails.title} />
+      <meta name="description" property="og:description" content={blogDetails.description} />
+      <meta name="keywords" property="og:keywords" content={blogDetails.keywords} />
+      <meta name="image" property="og:image" content={previewImgUrl} />
+      <meta name="type" property="og:type" content="article" />
+      <meta name="url" property="og:url" content={pageUrl} />
       <div className="blog-text" dangerouslySetInnerHTML={{ __html: blogText }} />
     </div>
   );
